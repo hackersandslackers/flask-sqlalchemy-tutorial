43206327daa69577ff4042e3f6dfb783473f2b2f
Todd Birchard <toddbirchard@gmail.com>
2020-04-17 22:33:25 -0400

    Minor formatting.

--- application/__init__.py
+++ application/__init__.py
@@ -8,13 +8,11 @@ def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
+
     db.init_app(app)
 
     with app.app_context():
-        # Imports
-        from . import routes
-
-        # Create tables for our models
-        db.create_all()
+        from . import routes  # Import routes
+        db.create_all()  # Create database tables for our data models
 
         return app
