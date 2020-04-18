10b97e83f63031e3adcc4b343ac9ca3153ee3171
Todd Birchard <toddbirchard@gmail.com>
2020-04-17 22:22:48 -0400

    Simplified config.

--- config.py
+++ config.py
@@ -6,13 +6,15 @@ load_dotenv(path.join(basedir, '.env'))
 
 
 class Config:
-    """Set Flask configuration vars from .env file."""
+    """Set Flask configuration variables from .env file."""
 
-    # General Config
+    # General Flask Config
     SECRET_KEY = environ.get('SECRET_KEY')
-    FLASK_APP = environ.get('FLASK_APP')
     FLASK_ENV = environ.get('FLASK_ENV')
+    FLASK_APP = 'wsgi.py'
+    FLASK_DEBUG = 1
 
     # Database
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@
 
 export FLASK_APP=wsgi.py
 export FLASK_DEBUG=1
-export APP_CONFIG_FILE=config.ini
 flask run
