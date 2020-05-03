14495399f36a19b11c4a1991ac672ccc9f6e3ba4
Todd Birchard <toddbirchard@gmail.com>
2020-05-02 22:58:57 -0400

    Removed SQLAlchemy echo.

--- config.py
+++ config.py
@@ -16,5 +16,5 @@ class Config:
 
     # Database
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
+"""App entry point."""
 from application import create_app
 
 app = create_app()
